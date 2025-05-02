52261f71ed3e994547ec12f2a41146495de0fcda
ShayAbergil <157501181+ShayAbergil@users.noreply.github.com>
2025-05-02 00:13:57 +0000

    img fix2

--- src/components/HeaderImage.jsx
+++ src/components/HeaderImage.jsx
@@ -2,21 +2,31 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import './HeaderImage.css';
 
+// Import images from the Header_img folder
+import Header_home from 'Header_img/Header_home.png';
+import Header_statistic from 'Header_img/Header_statistic.png';
+import Header_oq_cut from 'Header_img/Header_oq_cut.png';
+import Header_subject from 'Header_img/Header_subject.png';
+import Header_dq from 'Header_img/Header_dq.png';
+import Header_end from 'Header_img/Header_end.png';
+import Header_clean from 'Header_img/Header_clean.png';
+
 function HeaderImage() {
   const location = useLocation();
   const path = location.pathname;
 
-  // Define a map of paths to image filenames
+  // Define a map of paths to imported images
   const imageMap = {
-    "/": "Header_img/Header_home.png",
-    "/Statistic_questions": "Header_img/Header_statistic.png",
-    "/Overview_questions": "Header_img/Header_oq_cut.png",
-    "/Select_subject": "Header_img/Header_subject.png",
-    "/Detailed_questions": "Header_img/Header_dq.png",
-    "/Ending_screen": "Header_img/Header_end.png",
+    "/": Header_home,
+    "/Statistic_questions": Header_statistic,
+    "/Overview_questions": Header_oq_cut,
+    "/Select_subject": Header_subject,
+    "/Detailed_questions": Header_dq,
+    "/Ending_screen": Header_end,
   };
 
-  const headerImage = imageMap[path] || "Header_img/Header_clean.png";
+  const headerImage = imageMap[path] || Header_clean;
+
   return (
     <div className="header-image-container">
       <img src={headerImage} alt="Header" className="header-image" />
