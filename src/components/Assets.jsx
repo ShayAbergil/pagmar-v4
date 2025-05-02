60c20a0c854248ca8464d4d644c6855f94371f71
ShayAbergil <157501181+ShayAbergil@users.noreply.github.com>
2025-05-02 14:46:26 +0000

    addional ui updates - no rls yet

---
+++ src/components/Assets.jsx
@@ -0,0 +1,18 @@
+import './Assets.css';
+
+
+import t_dark_green from '/Assets/t_dark_green.png';
+import t_light_green from '/Assets/t_light_green.png';
+import t_mosaic from '/Assets/t_mosaic.png';
+import t_pink from '/Assets/t_pink.png';
+import t_yellow from '/Assets/t_yellow.png';
+
+const images = {
+    t_dark_green,
+    t_light_green,
+    t_mosaic,
+    t_pink,
+    t_yellow,
+  };
+  
+  export default images;
